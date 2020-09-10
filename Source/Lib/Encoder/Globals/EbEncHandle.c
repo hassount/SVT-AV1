fb34905e9b24ec030a03779a9a411997b410e625
hassount <hassene.tmar@intel.com>
2020-09-10 11:35:49 -0700

    Presets 4-7 mt tuning

--- Source/Lib/Encoder/Globals/EbEncHandle.c
+++ Source/Lib/Encoder/Globals/EbEncHandle.c
@@ -1960,7 +1960,7 @@ void set_param_based_on_input(SequenceControlSet *scs_ptr)
         (scs_ptr->static_config.enable_tpl_la && scs_ptr->input_resolution == INPUT_SIZE_240p_RANGE))
         scs_ptr->static_config.super_block_size = 64;
     else
-        scs_ptr->static_config.super_block_size = (scs_ptr->static_config.enc_mode <= ENC_M4) ? 128 : 64;
+        scs_ptr->static_config.super_block_size = (scs_ptr->static_config.enc_mode < ENC_M4) ? 128 : 64;
     scs_ptr->static_config.super_block_size = (scs_ptr->static_config.rate_control_mode > 0) ? 64 : scs_ptr->static_config.super_block_size;
    // scs_ptr->static_config.hierarchical_levels = (scs_ptr->static_config.rate_control_mode > 1) ? 3 : scs_ptr->static_config.hierarchical_levels;
     if (use_output_stat(scs_ptr))
